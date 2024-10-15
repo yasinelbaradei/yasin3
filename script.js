bd2f11062a541ff05331c62a3c629c7080feb79d
yasinelbaradei <yasinelbaradei@gmail.com>
2024-10-15 20:27:06 +0300

    delete notes "part 1"

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const noteInput = document.getElementById("notesInput");
+const notesDiv = document.getElementById("notesDiv")
+const zeroNotes = document.getElementById("zeroNotes")
 
 var notesArray = [];
 
@@ -14,19 +16,56 @@ function addNote() {
     const newNote = noteInput.value;
     noteInput.value = "";
     notesArray.push(newNote);
-    console.log(notesArray);
+    saveNotes();
 }
 
 function saveNotes() {
     const notesString = JSON.stringify(notesArray);
     localStorage.setItem("notes", notesString);
+    getNotes()
 }
 
 function getNotes() {
-    const notesString = localStorage.getItem("notes")
+    const notesString = localStorage.getItem("notes");
     if (notesString == null) {
         console.log("no note found");   
     } else {
-        notesArray = JSON.parse(notesString)
+        notesArray = JSON.parse(notesString);
+        if (notesArray.length > 0) {
+            zeroNotes.style.display = "none";
+            notesDiv.style.display = "block";
+            showNotes()
+        } else {
+            zeroNotes.style.display = "none";
+            notesDiv.style.display = "block";
+        }
     }
 }
+
+function showNotes() {
+
+    notesDiv.innerHTML = ""
+
+    for (let index = 0; index < notesArray.length; index++) {
+        const element = notesArray[index];
+
+        const newDiv = document.
+
+        createElement("div");
+        const newP = document.createElement("p");
+
+        newP.innerText = element;
+
+        newDiv.appendChild(newP);
+
+        const newIcon = document.createElement("i");
+
+        newIcon.className = "bi bi-trash";
+
+        newDiv.appendChild(newIcon);
+
+        notesDiv.appendChild(newDiv);
+    }
+}
+
+getNotes();
