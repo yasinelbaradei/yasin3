7da8661b0a1b570ef2827ce07fb43bd4dc60f7ab
yasinelbaradei <yasinelbaradei@gmail.com>
2024-10-17 15:58:52 +0300

    download + upload notes

--- script.js
+++ script.js
@@ -3,20 +3,33 @@ const notesDiv = document.getElementById("notesDiv");
 const zeroNotes = document.getElementById("zeroNotes");
 const optionDiv = document.getElementById("optionDiv");
 const loginDiv = document.getElementById("loginDiv");
-
+const loginEmail = document.getElementById("loginEmail");
+const loginPassword = document.getElementById("loginPassword");
+const loginError = document.getElementById("loginError");
+const detailsDiv = document.getElementById("detailsDiv");
+const greetingText = document.getElementById("greetingText");
 
 var notesArray = [];
 
 function showOption() {
-    optionDiv.style.display = "flex";
+    const userId = localStorage.getItem("userId");
+    if (userId == null) {
+        optionDiv.style.display = "flex";
+    } else {
+        const userName = localStorage.getItem("userName");
+        detailsDiv.style.display = "flex";
+        greetingText.innerText = `welcome ${userName}`;
+    }
+
     notesDiv.style.display = "none";
-    zeroNotes.style.display = "none";
+    zeroNotes.style.display = "none";    
 }
 
 function showHome() {
     optionDiv.style.display = "none";
     notesDiv.style.display = "block";
     loginDiv.style.display = "none"
+    detailsDiv.style.display = "none"
     getNotes()
 }
 
@@ -49,7 +62,9 @@ function saveNotes() {
 function getNotes() {
     const notesString = localStorage.getItem("notes");
     if (notesString == null) {
-        console.log("no note found");   
+        console.log("no note found"); 
+        zeroNotes.style.display = "block";
+        notesDiv.style.display = "none";
     } else {
         notesArray = JSON.parse(notesString);
         if (notesArray.length > 0) {
@@ -58,8 +73,8 @@ function getNotes() {
         } else {
             zeroNotes.style.display = "block";
             notesDiv.style.display = "none";
+            showNotes();
         }
-        showNotes()
     }
 }
 
@@ -90,3 +105,81 @@ function showNotes() {
 }
 
 getNotes();
+
+async function loginUser() {
+
+    loginError.style.display = "none"
+
+    const APIUrl = `https://tatbeqak.site/apps/tatbeqey/apps/easynotes/login?email=${loginEmail.value}&password=${loginPassword.value}`;
+
+    const response = await fetch(APIUrl);
+
+    const data = response.json();
+
+    const status = data.status;
+
+    if (status == true) {
+        const userName = data.name;
+        const userId = data.id;
+
+        localStorage.setItem("userName", userName);
+        localStorage.setItem("userId", userId);
+
+        greetingText.innerText = `welcome ${userName}`;
+
+        loginDiv.style.display = "none";
+        detailsDiv.style.display = "flex";
+    } else {
+        loginError.style.display = "block";
+    }
+    
+}
+
+function logOutUser() {
+    localStorage.removeItem("userId");
+    localStorage.removeItem("userName");
+    showHome();
+}
+
+
+async function UpLoadNotes() {
+    const userId = localStorage.getItem("userId");
+    const notesString = JSON.stringify(notesArray);
+
+    const APIUrl = `https://tatbeqak.site/apps/tatbeqey/apps/easynotes/addnote?id=${userId}&notes=${notesString}`;
+
+    const respones = await fetch(APIUrl);
+
+    const data = respones.json();
+
+    const status = data.status;
+
+    if (status == true) {
+        alert("notes Uploaded successfully")
+    } else {
+        alert("notes Upload failed!")
+    }
+    
+}
+
+async function downLoadNotes() {
+    const userId = localStorage.getItem("userId");
+
+    const APIUrl = `https://tatbeqak.site/apps/tatbeqey/apps/easynotes/getnotes?id=${userId}`;
+
+    const response = await fetch(APIUrl);
+
+    const data = response.json();
+
+    const status = data.status;
+
+    if (status == true) {
+        const notesString = data.notes;
+        notesArray = JSON.parse(notesString);
+        detailsDiv.style.display = "none"
+        saveNotes();
+        alert("notes downloaded successfully");
+    } else {
+        alert("notes download failed!");
+    }
+}
